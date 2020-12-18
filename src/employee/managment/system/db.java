76d5bf6a0846a48d6468fb57edbc33b74549f20a
Ahmed Kralani <ak42549@ubt-uni.net>
2020-12-18 23:45:32 +0100

    Implemented Login System for Admin
    Signed-off-by: Ahmed Kralani <ak42549@ubt-uni.net>

--- src/employee/managment/system/db.java
+++ src/employee/managment/system/db.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package employee.managment.system;
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ak-mi
  */
 public class db {
+    
+    Connection conn = null;
+    public static Connection java_db() {
+        try {
+            
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\Employee Managment System\\employeedb.sqlite");
+            return conn;
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
     
 }
